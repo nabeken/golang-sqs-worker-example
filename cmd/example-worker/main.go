eeeaccb98d5c5551be1c3105898a148f0637bb4c
TANABE Ken-ichi <nabeken@tknetworks.org>
2014-11-26 10:03:31 +0900

    cmd: Add -q flag to specify a queue name

--- cmd/example-worker/main.go
+++ cmd/example-worker/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"time"
@@ -11,19 +12,23 @@ import (
 	"github.com/nabeken/golang-sqs-worker-example/worker"
 )
 
+var flagQueue = flag.String("q", "example", "specify a queue name")
+
 func Print(msg *sqs.Message) error {
 	fmt.Println(msg.Body)
 	return nil
 }
 
 func main() {
+	flag.Parse()
+
 	auth, err := aws.GetAuth("", "", "", time.Now())
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	s := sqs.New(auth, aws.APNortheast)
-	queue, err := worker.NewSQSQueue(s, "example")
+	queue, err := worker.NewSQSQueue(s, *flagQueue)
 	if err != nil {
 		log.Fatal(err)
 	}
